b5f73b80b8e2416aa45a353aca8aa3f5b2ab3282
Indra Lascu <indra.lascu@nexttech.ro>
2018-04-25 16:49:55 +0300

    The differences between old javascript and es6

--- main.ts
+++ main.ts
@@ -1,5 +1,54 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// Old Javascript code
+function ShoppingList() {
+    this.groceries = [];
+}
 
-import { AppModule } from './app/app.module';
+ShoppingList.prototype.addItem = function(item) {
+    this.groceries = this.groceries.concat(item); //Immutable way of doing it
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+ShoppingList.prototype.removeItem = function(item) {
+    this.groceries = this.groceries.filter(function(grocery) {
+        return grocery !== item; // Banana !== Banana
+    });
+}
+
+var mylist = new ShoppingList();
+
+mylist.addItem('Banana');
+mylist.addItem('Apple');
+console.log(mylist.groceries);
+
+mylist.removeItem('Banana');
+
+console.log(mylist.groceries);
+
+// Typescript way
+
+class ShoppingList2 {
+    //no function call
+    //property needs to be added here before intializing it in the constructor
+    groceries: string[];
+
+    constructor() {
+        this.groceries = [];
+    }
+
+    //no need to add to the prototype
+    //no need to use the function keyword;
+    addItem(item) {
+        //es6 has the spread operator
+        this.groceries = [...this.groceries, item];
+    }
+
+    removeItem(item) {
+        this.groceries = this.groceries.filter((grocery) =>  item !== grocery ); //implicit return
+    }
+
+}
+const myNewList = new ShoppingList2();
+
+myNewList.addItem('Pear');
+myNewList.addItem('Pizza');
+
+console.log(myNewList.groceries);
