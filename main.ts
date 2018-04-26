964d647e9cc52242b6c986d02a1893e85757e8c6
Indra Lascu <indra.lascu@nexttech.ro>
2018-04-26 16:38:00 +0300

    import/export example

--- main.ts
+++ main.ts
@@ -1,54 +1,5 @@
-// Old Javascript code
-function ShoppingList() {
-    this.groceries = [];
-}
+import { uppercase } from "./app/formatter";
 
-ShoppingList.prototype.addItem = function(item) {
-    this.groceries = this.groceries.concat(item); //Immutable way of doing it
-}
+let myName: string = "Todd";
 
-ShoppingList.prototype.removeItem = function(item) {
-    this.groceries = this.groceries.filter(function(grocery) {
-        return grocery !== item; // Banana !== Banana
-    });
-}
-
-var mylist = new ShoppingList();
-
-mylist.addItem('Banana');
-mylist.addItem('Apple');
-console.log(mylist.groceries);
-
-mylist.removeItem('Banana');
-
-console.log(mylist.groceries);
-
-// Typescript way
-
-class ShoppingList2 {
-    //no function call
-    //property needs to be added here before intializing it in the constructor
-    groceries: string[];
-
-    constructor() {
-        this.groceries = [];
-    }
-
-    //no need to add to the prototype
-    //no need to use the function keyword;
-    addItem(item) {
-        //es6 has the spread operator
-        this.groceries = [...this.groceries, item];
-    }
-
-    removeItem(item) {
-        this.groceries = this.groceries.filter((grocery) =>  item !== grocery ); //implicit return
-    }
-
-}
-const myNewList = new ShoppingList2();
-
-myNewList.addItem('Pear');
-myNewList.addItem('Pizza');
-
-console.log(myNewList.groceries);
+console.log(uppercase(myName));
