73df46880dd105c9e3bf38eb3116ab41c1fdfdad
Indra Lascu <indra.lascu@nexttech.ro>
2018-05-16 16:32:13 +0300

    Bootstrapping a component

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+//first step is to import the module
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+//tell angular how to bootstrap this
+// to compile in the browser
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; //this contains the clientside code that processes our templates and all of our bindings
+
+platformBrowserDynamic() .bootstrapModule(AppModule);
