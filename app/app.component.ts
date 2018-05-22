49d90397c0cd7236cb77998ed52e38246abc0451
Indra Lascu <indra.lascu@nexttech.ro>
2018-05-22 16:36:37 +0300

    ngIf explanations

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,19 +5,28 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-    <button (click)="handleClick(username.value)">
-      Change me
-    </button>
       <input 
-        type="text" #username>
-      <div> {{ name }} </div>
-    </div>
-  `
-})
-export class AppComponent { // this allows us to import it in the app.module.ts
-  name: string = "Indra";
+        type="text"
+        [value]="name"
+        (input)="handleChange($event.target.value)">
 
-  handleClick(value: string) {
+      <template [ngIf]="name.length"> 
+        <div>
+          Searching for ... {{ name }} 
+        </div>
+      </template>
+      <div *ngIf="name.length"> 
+        Searching for ... {{ name }} 
+      </div>
+    </div>
+  ` 
+})
+// the ngIf is actually saved inside the template
+//Web components
+export class AppComponent { // this allows us to import it in the app.module.ts
+  name: string = '' ;
+
+  handleChange(value: string) {
     this.name = value;
   }
 }
